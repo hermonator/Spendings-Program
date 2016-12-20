3da17300930c13bc91f4ab36d4ac2ce195305268
Jesse <jesse.hermon@my.jcu.edu.au>
2016-12-20 17:11:04 +1000

    Initial file were places. A receipt class was created for the details of a receipt. A Receipts class was created to store multiple receipts. A Run class was created to run thge application.

---
+++ Main/src/Receipt.java
@@ -0,0 +1,5 @@
+/**
+ * Created by jc259420 on 20/12/2016.
+ */
+public class Receipt {
+}
---
+++ Main/src/Run.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jc259420 on 20/12/2016.
+ */
+public class Run {
+
+    public static void main(String[] args) {
+
+    }
+}
